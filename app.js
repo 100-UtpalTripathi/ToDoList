5a5219e120efe1d03dac027326fdad1bb7d9160a
100-UtpalTripathi <utpaltripathi14@gmail.com>
2023-03-09 17:13:22 +0530

    Second commit

--- app.js
+++ app.js
@@ -4,13 +4,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
 
   var today = new Date();
-  if (today.getDay() == 1 || today.getDay() == 0) {
-    res.sendFile(__dirname + '/index.html');
+  var currentDay = today.getDay();
+  
+  today = "";
+  switch (currentDay) {
+    case 0: today = "Sunday"; break;
+    case 1: today = "Monday"; break;
+    case 2: today = "Tuesday"; break;
+    case 3: today = "Wednesday"; break;
+    case 4: today = "Thursday"; break;
+    case 5: today = "Friday"; break;
+    case 6: today = "Saturday"; break;
   }
+
+
+  res.render('list', { kindOfDay: today });
+
 });
 
 app.listen(3000, function () {
