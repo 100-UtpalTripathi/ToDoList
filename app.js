dbe17a1fcc50f206287688dc9eef859c7922ebeb
100-UtpalTripathi <utpaltripathi14@gmail.com>
2023-03-12 11:41:38 +0530

    Version 1 Completed

--- app.js
+++ app.js
@@ -6,25 +6,39 @@ const bodyParser = require('body-parser');
 const app = express();
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [];
+let workItems = [];
+
 app.get('/', function (req, res) {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  var day = today.toLocaleDateString("en-US", options);
+  res.render('list', { listTitle: day, newListItems: items });
+});
 
-  var today = new Date();
-  var currentDay = today.getDay();
-  
-  today = "";
-  switch (currentDay) {
-    case 0: today = "Sunday"; break;
-    case 1: today = "Monday"; break;
-    case 2: today = "Tuesday"; break;
-    case 3: today = "Wednesday"; break;
-    case 4: today = "Thursday"; break;
-    case 5: today = "Friday"; break;
-    case 6: today = "Saturday"; break;
+app.get('/work', function (req, res) {
+  res.render('list', { listTitle: "Work List", newListItems: workItems });
+});
+
+//in the form declared in list.ejs, action is defined as "/", so to differentiate whether data is coming from '/' or '/work' we need to use button value. In list.ejs, button value we are setting dynamically, which is passed to post '/' and there we have to decide whether it is coming from '/' or '/work' so accordingly we are adding to the lists and redirecting.....
+app.post('/', function (req, res) {
+  let item = req.body.newItem;
+  //console.log(req.body);
+  if (req.body.button === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
   }
-
-
-  res.render('list', { kindOfDay: today });
-
+  else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
 app.listen(3000, function () {
